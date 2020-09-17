1a2dfef6577055bb7914bad58b23b6194508022b
razorvlades <charlie.pierce17@gmail.com>
2020-09-16 19:18:09 -0500

    added editing of apps

--- src/Settings.js
+++ src/Settings.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useStores } from './stores';
 import { observer } from 'mobx-react';
+import ContentEditable from 'react-contenteditable'
 
 const Settings = observer((props) => {
 
@@ -10,125 +11,171 @@ const Settings = observer((props) => {
     const settingsContainerStyle = {
         alignContent: 'center',
         marginTop: 25,
-        marginLeft: 10,
-        marginRight: 10,
+        marginLeft: 200,
+        marginRight: 200,
         borderRadius: 10,
         backgroundColor: 'white',
-        display: 'flex',
-        flexDirection: 'column',
         justifyContent: 'center',
-        overflow: 'hidden'
+        overflow: 'hidden',
     }
 
     const itemStyle = {
-        flexDirection: 'row',
-        display: 'flex',
-        backgroundColor: 'gray',
-        alignContent: 'center',
-        justifyContent: 'center',
-        paddingBottom: 10,
+        backgroundColor: '#F2F3F6',
         paddingTop: 10,
+        paddingBottom: 10,
+        height: 40,
     }
 
     const nameStyle = {
         flex: 2,
-        backgroundColor: 'blue',
         paddingLeft: 15
     }
     const urlStyle = {
         flex: 5,
-        backgroundColor: 'green',
         paddingLeft: 15
     }
     const editStyle = {
         flex: 1,
-        backgroundColor: 'red',
         paddingLeft: 15
     }
     const deleteStyle = {
         flex: 1,
-        backgroundColor: 'orange',
         paddingLeft: 15
     }
 
+    const tableHeaderStyle = {
+        paddingTop: 12,
+        paddingBottom: 12,
+        paddingLeft: 15,
+        textAlign: 'left',
+        backgroundColor: '#F2F3F6',
+    }
+
+    const tableHeaderContainerStyle = {
+        width: '100%',
+    }
+
+    const tableStyle = {
+        textAlign: "left",
+        borderCollapse: "collapse",
+        width: "100%"
+    }
+
     return (
-      <div style={settingsContainerStyle}>
-            <div style={itemStyle}>
-                <div style={nameStyle}>
-                    Name
-                </div>
-                <div style={urlStyle}>
-                    URL
-                </div>
-                <div style={editStyle}>
-                    Edit
-                </div>
-                <div style={deleteStyle}>
-                    Delete
-                </div>
-          </div>
-        { 
-            appList.map((app, index) => (
-                <SettingsAppItem key={app.name + index} app={app}/>
-            ))
-        }
-      </div>
+        <div style={settingsContainerStyle}>
+            <table style={tableStyle}>
+                <thead style={tableHeaderContainerStyle}>
+                    <tr style={itemStyle}>
+                        <th style={tableHeaderStyle}>Name</th>
+                        <th style={tableHeaderStyle}>URL</th>
+                        <th style={tableHeaderStyle}>Edit</th>
+                        <th style={tableHeaderStyle}>Delete</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {
+                        appList.map((app, index) => (
+                            <SettingsAppItem key={app.name + index} index={index} app={app}/>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </div>
     )
 });
 
-const SettingsAppItem = (props) => {
+const SettingsAppItem = observer((props) => {
 
     const {
-        app
+        app,
+        index
     } = props;
 
+    const name = useRef(app.name);
+    const url = useRef(app.url);
+    const [editing, setEditing] = useState(false);
+
+    const [hover, setHover] = useState(false);
+
+    const _toggleHover = () => setHover(!hover);
+
     const itemStyle = {
-        flexDirection: 'row',
-        display: 'flex',
-        backgroundColor: 'pink',
-        alignContent: 'center',
-        justifyContent: 'center',
+        backgroundColor: hover ? '#F2F3F6' : 'white',
         paddingTop: 10,
-        paddingBottom: 10
+        paddingBottom: 10,
+        cursor: 'pointer',
+        height: 40
     }
 
     const nameStyle = {
-        flex: 2,
-        backgroundColor: 'blue',
-        paddingLeft: 15
+        paddingLeft: 15,
+        overflow:'wrap',
     }
     const urlStyle = {
-        flex: 5,
-        backgroundColor: 'green',
-        paddingLeft: 15
+        paddingLeft: 15,
+        overflow:'wrap',
     }
     const editStyle = {
-        flex: 1,
-        backgroundColor: 'red',
-        paddingLeft: 15
+        overflow:'wrap',
+        paddingLeft: 15,
     }
     const deleteStyle = {
-        flex: 1,
-        backgroundColor: 'orange',
-        paddingLeft: 15
+        overflow:'wrap',
+        paddingLeft: 15,
+    }
+
+    const handleChange = evt => {
+        name.current = evt.target.value;
+    };
+    
+    const handleBlur = () => {
+        console.log(name.current);
+    };
+
+    const _handleURLChange = evt => {
+        url.current = evt.target.value;
+    };
+    
+    const _handleURLBlur = () => {
+        console.log(url.current);
+    };
+
+    const _toggleEditing = () => {
+        if (editing) {
+            saveApps();
+        }
+        setEditing(!editing);
+    }
+
+    const { globalStore } = useStores();
+
+    const saveApps = () => {
+        let newApps = [...globalStore.apps];
+        newApps[index] = {
+            name: name.current,
+            url: url.current,
+            icon: newApps[index].icon
+        }
+        globalStore.setApps(newApps);
     }
 
     return (
-        <div style={itemStyle}>
-            <div style={nameStyle}>
-                {app.name}
-            </div>
-            <div style={urlStyle}>
-                {app.url}
-            </div>
-            <div style={editStyle}>
-                    Edit
-            </div>
-            <div style={deleteStyle}>
-                Delete
-            </div>
-        </div>
+        <tr onMouseEnter={_toggleHover} onMouseLeave={_toggleHover} style={itemStyle}>
+            <td style={nameStyle}>
+                <ContentEditable disabled={!editing} html={name.current} onBlur={handleBlur} onChange={handleChange} />
+            </td>
+            <td style={urlStyle}>
+                <ContentEditable disabled={!editing} html={url.current} onBlur={_handleURLBlur} onChange={_handleURLChange} />
+            </td>
+            <td style={editStyle}>
+                <button onClick={_toggleEditing}>
+                    { !editing ? 'Edit' : 'Save'}
+                </button>
+            </td>
+            <td style={deleteStyle}>Delete</td>
+        </tr>
     )
-}
+});
 
 export default Settings;
