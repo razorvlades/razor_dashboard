9945143665a8ec7f23d33201b918704e585b4d09
Charlie Pierce <charlie.pierce17@gmail.com>
2020-09-21 12:51:39 -0500

    added user login and account creation to server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,37 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const fs = require('fs');
 const fs_extra = require('fs-extra')
 const multer = require('multer');
+const crypto = require('crypto');
+const { MongoClient } = require("mongodb");
 
+// get current directory
 const path = require('path');
 const dir = __dirname + '/';
+
+// express server settings
+const PORT = proces.env.PORT || 9078;
+const HOST = proces.env.HOST || '0.0.0.0';
+
+// mongodb settings and connection
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const DB_HOST = process.env.DB_HOST;
+const DB_NAME = process.env.DB_NAME;
+const DB_URI = "mongodb://" + DB_USER + ':' + DB_PASS + "@" + DB_HOST;
+const DB_OPTIONS = {
+    useUnifiedTopology: true
+}
+const client = new MongoClient(DB_URI, DB_OPTIONS);
+
+async function connect_db() {
+    await client.connect();
+    await client.db(DB_NAME).command({ ping: 1 });
+    console.log("Connected successfully to server");
+}
+connect_db();
 
 if (!fs.existsSync(dir + '../config')) {
     fs.mkdirSync(dir + '../config', { recursive: true });
@@ -75,8 +101,92 @@ app.post("/upload-image", upload.single('imageData'), (req, res, next) => {
     res.send({ ok: true })
 });
 
-const PORT = 9078;
-const HOST = '0.0.0.0';
+app.post("/login", async (req, res) => {
+    console.log("/login POST request received");
+
+    let rc = await user_login({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    return res.status(rc.status).json({ message: rc.message, user: rc.user });
+});
+
+app.post('/create_user', async (req, res) => {
+    console.log("/create_user POST request received");
+    
+    let rc = await create_user({
+        username: req.body.username,
+        password: req.body.password,
+        image: ''
+    });
+
+    return res.status(rc.status).json({ message: rc.message, user: rc.user });
+});
+
+async function user_login(user) {
+    if (!user) {
+      return { status: 400, message: "Must specify a user"};
+    }
+    if (!user.username || !user.password)
+      return { status: 400, message: "No username or password specified"};
+
+    let db = client.db(DB_NAME);
+  
+    let result = await db.collection("users").findOne({ username: user.username }, {});
+  
+    if (!result)
+      return { status: 404, message: "User does not exist", user: user};
+    
+    if (!checkPass(user.password, result.password))
+      return { status: 401, message: "Incorrect password", user: user};
+  
+    delete result.password;
+    
+    return { status: 200, message: "User authenticated successfully", user: result};
+}
+
+async function create_user(user) {
+    if (!user) {
+      return { status: 400, message: "Must specify a user"};
+    }
+    if (!user.password || !user.username)
+        return { status: 400, message: "Username and/or password is empty" };
+
+    let db = client.db(DB_NAME);
+
+    let result = await db.collection('users').findOne({username: user.username});
+    if (result)
+      return { status: 409, message: "Username already taken" };
+
+    user.password = encryptPass(user.password);
+
+    result = await db.collection('users').insertOne(user);
+
+    if (!result)
+      return { status: 500, message: "Error adding user to database" };
+
+    let user_result = result.ops[0];
+    delete user_result.password;
+
+    return { status: 200, message: "User successfully created", user: user_result };
+}
+
+function encryptPass(password) {
+    let salt = crypto.randomBytes(16).toString('base64');
+    let hash = crypto.createHmac('sha512', salt).update(password).digest("base64");
+    password = salt + "$" + hash;
+    return password;
+}
+
+function checkPass(user_pass, password) {
+    let passwordFields = password.split('$');
+    let salt = passwordFields[0];
+    let hash = crypto.createHmac('sha512', salt).update(user_pass).digest("base64");
+    if (hash === passwordFields[1])
+      return true;
+    return false;
+}
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
